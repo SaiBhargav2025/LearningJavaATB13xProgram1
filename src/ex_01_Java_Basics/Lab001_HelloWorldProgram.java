2567623d12f2fc21c62a242855163f8e32fd2e4a
SaiBhargav2025 <sanaboinasaibharav@gmail.com>
2025-06-15 17:16:29 +0530

    fist

--- src/ex_01_Java_Basics/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorldProgram.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basics;
 
-public class Lab_001 {
+public class Lab001_HelloWorldProgram {
        public static void main(String[] args) {
         System.out.println("Hello World!");
     }
