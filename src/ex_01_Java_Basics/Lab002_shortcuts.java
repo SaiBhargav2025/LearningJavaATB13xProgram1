becd7861f060c8e3e6b275095001d822b6dc5b2e
SaiBhargav2025 <sanaboinasaibharav@gmail.com>
2025-06-15 23:41:15 +0530

    Javabasics15

---
+++ src/ex_01_Java_Basics/Lab002_shortcuts.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab002_shortcuts {
+        public static void main(String[] args) {
+            System.out.println("Hello Test");
+        }
+
+        //    public static void main(String[] args) {
+//
+//    }
+        public static void main(int args) {
+
+        }
+    }
+
---
+++ src/ex_01_Java_Basics/sai_145.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class sai_145 {
+    public static void main(String[] ThiscanbeAnything) {
+        System.out.println("Hi");
+    }
+}
