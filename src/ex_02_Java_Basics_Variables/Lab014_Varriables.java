0858cf33f275a1c6609dedc70d25fdb5864656c8
SaiBhargav2025 <sanaboinasaibharav@gmail.com>
2025-06-17 22:33:12 +0530

    lab2

---
+++ src/ex_02_Java_Basics_Variables/Lab014_Varriables.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics_Variables;
+
+public class Lab014_Varriables {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main() {
+
+    }
+}
---
+++ src/ex_02_Java_Basics_Variables/Lab018.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Variables;
+
+public class Lab018 {
+    public static void main(String[] args) {
+//        int _ = 65;
+        int $ = 65;
+        int yest = 65;
+        int this_is_a_vari_long_name_in_the_class_hello_$_SAI = 89;
+    }
+
+}
