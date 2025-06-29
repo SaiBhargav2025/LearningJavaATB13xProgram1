748e062fc97453b00a825541b328e47182c57b93
SaiBhargav2025 <sanaboinasaibharav@gmail.com>
2025-06-29 21:50:00 +0530

    12th June

---
+++ src/ex_06_Ternary_Operator/Lab062_TO.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_TO {
+    public static void main(String[] args) {
+        //Age to Vote?
+        int age = 10;
+        String canIvote = age > 18 ? "yes" : "No";
+        System.out.println(canIvote);
+    }
+}
